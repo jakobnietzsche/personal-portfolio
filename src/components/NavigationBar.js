9393e7390700a045243ea2d967957df35c42df62
Lockmyre <51970698+Lockmyre@users.noreply.github.com>
2022-10-10 09:55:55 -0500

    added navigation bar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import linkedInIcon from '../assets/images/linkedin-icon.svg';
+import gitHubIcon from '../assets/images/github-icon.svg';
+import instagramIcon from '../assets/images/instagram-icon.svg';
+
+export const NavigationBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/"></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Experience</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="#"><img src={linkedInIcon} alt="" /></a>
+              <a href="#"><img src={gitHubIcon} alt="" /></a>
+              <a href="#"><img src={instagramIcon} alt="" /></a>
+            </div>
+            <button className="emailButton"><span>Email Me!</span></button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
